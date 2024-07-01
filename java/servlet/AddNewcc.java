85f8a670e564e940712b5d18ce13adb0c66ca1c2
nayandangeSDE27 <163569246+nayandangeSDE27@users.noreply.github.com>
2024-07-02 00:06:56 +0530

    Add files via upload

---
+++ java/servlet/AddNewcc.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import medicalcenter.database;
+
+public class AddNewcc extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String ccName = request.getParameter("cc_name");
+        String value = "'" + ccName + "','chief_complaint'";
+        try {
+            database db = new database();
+            db.insert("diagnosis_info", "observation_name,observation_type", value);
+        } catch (SQLException e) {
+            System.out.println("Add new cc = " + ccName);
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
